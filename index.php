ceb181af469b12450aaf44eda90fed062ce7f88a
Vikramjeet SIngh <vikramjeet@ucreate.co.in>
2018-03-29 15:20:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
-<?php
-echo 'Hello ';exit;
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>AWS</title>
+	</head>
+	<body class="landing">
+			<section id="banner">
+				<div class="inner">
+					<h2><?php echo 'Hello world on '.date('Y-m-d'); ?></h2>
+					
+				</div>
+			</section>
+	</body>
+</html>
