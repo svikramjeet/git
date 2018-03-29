841d644470de56a550d7ea22a9278f6e0a398747
Vikramjeet SIngh <vikramjeet@ucreate.co.in>
2018-03-29 16:56:04 +0530

    Update before_start.sh

--- before_start.sh
+++ before_start.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-
-php /var/www/html/artisan clear-compiled
-php /var/www/html/artisan optimize
-php /var/www/html/artisan view:clear
-php /var/www/html/artisan cache:clear
-
-chown -R ubuntu:www-data /var/www/html
+sudo chown -R ubuntu:www-data /var/www/html
 sudo find /var/www/html -type d -exec chmod 755 {} +
 sudo find /var/www/html -type f -exec chmod 644 {} +
-chmod -R 777 /var/www/html/storage
+sudo chmod -R 777 /var/www/html/storage
 
 composer update -d /var/www/html
