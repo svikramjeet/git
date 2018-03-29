17d0c903bb85854a1deb7855d0315d304ca0c751
Vikramjeet SIngh <vikramjeet@ucreate.co.in>
2018-03-29 17:05:42 +0530

    Update before_start.sh

--- before_start.sh
+++ before_start.sh
@@ -4,4 +4,4 @@ sudo find /var/www/html -type d -exec chmod 755 {} +
 sudo find /var/www/html -type f -exec chmod 644 {} +
 sudo chmod -R 777 /var/www/html/storage
 
-composer update -d /var/www/html
+sudo composer install -d /var/www/html
