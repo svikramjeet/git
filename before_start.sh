a834d905be73af8e14ad55f4805c2e4ebb324dd9
Vikramjeet SIngh <vikramjeet@ucreate.co.in>
2018-03-29 16:53:03 +0530

    Update before_start.sh

--- before_start.sh
+++ before_start.sh
@@ -1,2 +1,13 @@
-#!/bin/sh
-composer install --no-plugins --no-scripts
+#!/bin/bash
+
+php /var/www/html/artisan clear-compiled
+php /var/www/html/artisan optimize
+php /var/www/html/artisan view:clear
+php /var/www/html/artisan cache:clear
+
+chown -R ubuntu:www-data /var/www/html
+sudo find /var/www/html -type d -exec chmod 755 {} +
+sudo find /var/www/html -type f -exec chmod 644 {} +
+chmod -R 777 /var/www/html/storage
+
+composer update -d /var/www/html
