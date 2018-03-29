81ddeccdcd3c0254e2e0b89cefc06edf50d9f3e9
Vikramjeet SIngh <vikramjeet@ucreate.co.in>
2018-03-29 17:18:04 +0530

    Update before_start.sh

--- before_start.sh
+++ before_start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-sudo chown -R ubuntu:www-data /var/www/html
-sudo find /var/www/html -type d -exec chmod 755 {} +
-sudo find /var/www/html -type f -exec chmod 644 {} +
-sudo chmod -R 777 /var/www/html/storage
-
-sudo composer install -d /var/www/html
+echo pwd
+cd /opt/composer
+composer install
+vendor/bin/phing www
