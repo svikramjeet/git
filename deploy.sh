15b67f0dcff3fbb1472a5948a063f0979ad6ddea
Vikramjeet SIngh <vikramjeet@ucreate.co.in>
2018-04-02 15:17:05 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+sudo apt-get install wget
 content=$(wget http://production-review-tool.herokuapp.com/api/checkReadyToDeploy?app_name=clientshare-web -q -O -)
 echo $content
 if [ "${content:1:14}" == '"success":true' ]; then 
