36b60019b24f0ba9cbaa7bba53b9d68ab1e8b6fc
Vikramjeet SIngh <vikramjeet@ucreate.co.in>
2018-04-25 12:11:05 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install wget
-content=$(wget http://production-review-tool.herokuapp.com/api/checkReadyToDeploy?app_name=clientshare-web -q -O -)
+content=$(wget http://production-review-tool.herokuapp.com/api/checkReadyToDeploy?app_name=webman -q -O -)
 echo $content
 if [ "${content:1:14}" == '"success":true' ]; then 
  exit 0
